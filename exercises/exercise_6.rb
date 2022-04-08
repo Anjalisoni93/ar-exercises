8a46ab8c8f36d10eb5a456022ed41dd6dd7af3c9
Anjali Soni <anjalipatadiya@gmail.com>
2022-04-08 16:19:00 -0600

    Add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id:4)
+@store1.employees.create(first_name: "Anjali", last_name: "Soni", hourly_rate: 60)
+@store1.employees.create(first_name: "Jigar", last_name: "Soni", hourly_rate: 100)
+@store2.employees.create(first_name: "Pratik", last_name: "Soni", hourly_rate: 80)
+@store3.employees.create(first_name: "Arwa", last_name: "Chawda", hourly_rate: 90)
